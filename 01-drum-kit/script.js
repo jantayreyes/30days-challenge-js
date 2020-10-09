dfb63daab104ccc18cc8b1520039e5d6d9cf02f4
jantayreyes <jantayreyes@gmail.com>
2020-10-09 15:06:08 +0200

    drum kit with letters from A to L

--- 01-drum-kit/script.js
+++ 01-drum-kit/script.js
@@ -2,8 +2,10 @@ function playMusic(e) {
   // When you press a keyword you have a keycode
   const music = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if(!music) return; // this stop the function from running all together
-  music.currentTime = 0;  //this is if you want to press the keyword multiple times
+
+  music.currentTime = 0;  //this is if you want to press the keyword multiple times to hear the song
   // you will have not cooldown to the music
   music.play();
   key.classList.add('playing');
